d47f091a1ddde612630b52755b55a1f5e9ccd292
Nestor <nestorlbp@gmail.com>
2021-10-18 20:56:54 -0500

    conteo correcto

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ numero = int(input("Ingrese el numero: "))
 
 #print("El numero es numero:", numero)
 
+cont = 0
 for i in range(1, numero+1):
-    print(i)
+    if numero % i == 0:
+        cont += 1
+
+print(cont)
